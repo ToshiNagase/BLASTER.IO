5332f95abc0e4479fbaa46dec6772474407a1e81
AbdurRahman Bhatti <arbdelta@gmail.com>
2018-03-30 11:27:13 -0700

    change

--- gametestnetwork/server.js
+++ gametestnetwork/server.js
@@ -51,6 +51,5 @@ io.on('connection', function(socket) {
 });
 
 setInterval(function() {
-  io.sockets.emit('
-  ', players); // Inifinite loop
+  io.sockets.emit('state', players); // Inifinite loop
 }, 1000 / 60);
